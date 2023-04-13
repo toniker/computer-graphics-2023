9f43c3d42bac226002f927f0dced0b851dcfb568
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-13 14:10:27 +0300

    Create linear interpolation function

--- interpolate_vectors.py
+++ interpolate_vectors.py
@@ -1,7 +1,25 @@
-def interpolate_vectors(p1, p2, V1, V2, xy, dim):
+import math
+
+import numpy as np
+
+
+def interpolate_vectors(p1, p2, v1, v2, xy, dim) -> np.ndarray:
+    x1, y1 = p1
+    x2, y2 = p2
+    x, y = 0, 0
+
     if dim == 1:
         x = xy
+        y = y1 + (x - x1) * (y2 - y1) / (x2 - x1)
     elif dim == 2:
         y = xy
+        x = x1 + (y - y1) * (x2 - x1) / (y2 - y1)
 
+    distance_to_p1 = math.sqrt((x - x1) ** 2 + (y - y1) ** 2)
+    distance_to_p2 = math.sqrt((x - x2) ** 2 + (y - y2) ** 2)
+    distance_ratio_to_p1 = distance_to_p2 / (distance_to_p1 + distance_to_p2)
+    distance_ratio_to_p2 = 1 - distance_ratio_to_p1
+    v = distance_ratio_to_p1 * v1 + distance_ratio_to_p2 * v2
+
+    return v
 
