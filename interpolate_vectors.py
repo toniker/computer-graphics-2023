a3127c82bc5d9a3e40fbc8825e938c4b654899f9
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-13 14:14:15 +0300

    Add doc comment

--- interpolate_vectors.py
+++ interpolate_vectors.py
@@ -4,6 +4,17 @@ import numpy as np
 
 
 def interpolate_vectors(p1, p2, v1, v2, xy, dim) -> np.ndarray:
+    """
+    Calculates the value V of a vector, by linearly interpolating between two given vectors. The calculated vector
+    must lie on the line between the given vectors.
+    :param p1: The [x, y] coordinates of the first vector
+    :param p2: The [x, y] coordinates of the second vector
+    :param v1: The value of the first vector
+    :param v2: The value of the second vector
+    :param xy: The x or y coordinate of the vector to be calculated
+    :param dim: The dimension of the vector to be calculated. 1 = x, 2 = y
+    :return: The value of the vector
+    """
     x1, y1 = p1
     x2, y2 = p2
     x, y = 0, 0
