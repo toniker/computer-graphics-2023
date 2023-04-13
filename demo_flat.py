c7a8ab8e02c7b21dd6c98163e5d7f5aae162782f
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-13 14:39:13 +0300

    Scaffold flats function

--- demo_flat.py
+++ demo_flat.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+
+def flats(canvas, vertices, vcolors):
+    average_color = np.mean(vcolors, axis=0)
+
+
 if __name__ == "__main__":
     verts2d = np.load("h1.npy", allow_pickle=True).tolist()['verts2d']
     vcolors = np.load("h1.npy", allow_pickle=True).tolist()['vcolors']
     faces = np.load("h1.npy", allow_pickle=True).tolist()['faces']
     depth = np.load("h1.npy", allow_pickle=True).tolist()['depth']
     print("Hello, World!")
-
