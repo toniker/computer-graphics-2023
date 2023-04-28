6c2f4b4850261fe578d600f17b240703919622f5
Antonis Keremidis <tkeremidis@gmail.com>
2023-04-28 18:39:46 +0300

    Add gouraud interpolation

--- shade_triangle.py
+++ shade_triangle.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from interpolate_vectors import interpolate_vectors
 from shade_types import shade_types
 
 
@@ -78,7 +79,51 @@ def flats(canvas, vertices, colors):
 
 
 def gouraud(canvas, vertices, colors):
-    x_min, y_min = np.min(vertices, axis=0)
-    x_max, y_max = np.max(vertices, axis=0)
+    edges = get_edges_from_vertices(vertices)
+    y_min = min(edges, key=lambda e: e.min_y).min_y
+    y_max = max(edges, key=lambda e: e.max_y).max_y
+
+    active_edges = np.array([], dtype=Edge)
+    for y in range(y_min, y_max + 1):
+        # Set active edges and vertices
+        for edge in edges:
+            if edge.min_y == y and edge.max_y != edge.min_y:
+                active_edges = np.append(active_edges, edge)
+            elif edge.max_y == y:
+                active_edges = np.delete(active_edges, np.where(active_edges == edge))
+
+        if len(active_edges) < 2:
+            return canvas
+
+        active_vertices = np.empty((0, 2))
+
+        for active_edge in active_edges:
+            x = active_edge.get_intersecting_x(y)
+            active_vertices = np.vstack((active_vertices, np.array([x, y])))
+
+        color1_a_index = \
+            np.equal(vertices, (active_edges[0].x1, active_edges[0].y1)).all(axis=1).nonzero()[0].tolist()[0]
+        color2_a_index = \
+            np.equal(vertices, (active_edges[0].x2, active_edges[0].y2)).all(axis=1).nonzero()[0].tolist()[0]
+        color_a = interpolate_vectors((active_edges[0].x1, active_edges[0].y1),
+                                      (active_edges[0].x2, active_edges[0].y2), colors[color1_a_index],
+                                      colors[color2_a_index], y, dim=2)
+
+        color1_b_index = \
+            np.equal(vertices, (active_edges[1].x1, active_edges[1].y1)).all(axis=1).nonzero()[0].tolist()[0]
+        color2_b_index = \
+            np.equal(vertices, (active_edges[1].x2, active_edges[1].y2)).all(axis=1).nonzero()[0].tolist()[0]
+        color_b = interpolate_vectors((active_edges[1].x1, active_edges[1].y1),
+                                      (active_edges[1].x2, active_edges[1].y2), colors[color1_b_index],
+                                      colors[color2_b_index], y, dim=2)
+
+        active_vertices = np.sort(active_vertices, axis=0)
+
+        for x in range(0, len(active_vertices) - 1, 2):
+            x1 = int(active_vertices[x][0])
+            x2 = int(active_vertices[x + 1][0])
+            x_min, x_max = min(x1, x2), max(x1, x2)
+            color = interpolate_vectors((x_min, y), (x_max, y), color_a, color_b, x, dim=1)
+            canvas[x_min:x_max, y, :] = color
 
     return canvas
