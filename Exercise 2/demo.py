204844831ad923638b45cb43884810faa03c016d
Antonis Keremidis <tkeremidis@gmail.com>
2023-05-24 15:04:10 +0300

    Modify rasterize function for speed

--- Exercise 2/demo.py
+++ Exercise 2/demo.py
@@ -27,24 +27,50 @@ def rotmat(theta, unit):
 
 
 def rotate_translate(cp, theta, unit, A, t):
-    r = rotmat(theta, unit)
+    """
+    rotates and translates a set of points
+    :param cp: set of points
+    :param theta: angle in degrees
+    :param unit: the unit vector
+    :param A: translation matrix
+    :param t: translation vertex
+    :return: the translated set of points
+    """
     if cp.ndim == 1:
         cp = cp.reshape((-1, 1))
 
     centered = cp - A.reshape((-1, 1))
+
+    r = rotmat(theta, unit)
     rotated = np.dot(r, centered)
+
     translated = rotated + A.reshape((-1, 1))
+
     return translated + t.reshape((-1, 1))
 
 
 def change_coordinate_system(cp, r, c0):
     """
     Returns the coordinates of a set of points in a new coordinate system.
+    :param cp: set of points
+    :param r: rotation matrix
+    :param c0: translation vector
+    :return: the coordinates of the points in the new coordinate system
     """
     return np.dot(r, cp - c0).T
 
 
 def pin_hole(f, cv, cx, cy, cz, p3d):
+    """
+    Calculates the 2D coordinates of a set of 3D points in the image plane of a pinhole camera.
+    :param f: the focal length of the camera
+    :param cv: the center of the camera
+    :param cx: the x-axis of the camera
+    :param cy: the y-axis of the camera
+    :param cz: the z-axis of the camera
+    :param p3d: the 3D points
+    :return: the 2D coordinates of the points in the image plane
+    """
     r = np.vstack((cx.T, cy.T, cz.T))
 
     p3d_ccs = change_coordinate_system(p3d, r, cv)
@@ -59,6 +85,15 @@ def pin_hole(f, cv, cx, cy, cz, p3d):
 
 
 def camera_looking_at(f, cv, ck, cup, p3d):
+    """
+    calculates the 2D coordinates of a set of 3D points in the image plane of a camera
+    :param f: the focal length of the camera
+    :param cv: the center of the camera
+    :param ck: the target of the camera
+    :param cup: the up vector of the camera
+    :param p3d: the 3D points
+    :return: the 2D coordinates of the points in the image plane
+    """
     _ck = np.array(ck - cv)
     cz = _ck / np.linalg.norm(_ck)
     tilt = np.array(cup - np.dot(cz.flatten(), cup.flatten()) * cz)
@@ -69,21 +104,31 @@ def camera_looking_at(f, cv, ck, cup, p3d):
 
 
 def rasterize(p2d, rows, columns, h, w):
+    """
+    Rasterizes the 2D points
+    :param p2d: the 2d points
+    :param rows: the number of vertical pixels
+    :param columns: the number of horizontal pixels
+    :param h: the height of the camera
+    :param w: the width of the camera
+    :return: the rasterized 2D points
+    """
     vertical_ppi = rows / h
     horizontal_ppi = columns / w
 
-    n2d = np.zeros((p2d.shape[1], 2))
-    for i in range(p2d.shape[1]):
-        n2d[i, 0] = int(p2d[0, i] * horizontal_ppi)
-        n2d[i, 1] = int(p2d[1, i] * vertical_ppi)
+    n2d = p2d
+    n2d[0, :] *= horizontal_ppi
+    n2d[1, :] *= vertical_ppi
 
-    x_offset = int(columns / 2) - int(p2d[:, 0].mean())
-    y_offset = int(rows / 2) - int(p2d[:, 1].mean())
+    n2d = n2d.astype(int)
 
-    n2d[:, 0] += x_offset
-    n2d[:, 1] += y_offset
+    x_offset = int(columns / 2) - int(n2d[1, :].mean())
+    y_offset = int(rows / 2) - int(n2d[0, :].mean())
 
-    return n2d
+    n2d[0, :] += x_offset
+    n2d[1, :] += y_offset
+
+    return n2d.T
 
 
 def render(n2d, faces, colors, depth, rows, columns):
