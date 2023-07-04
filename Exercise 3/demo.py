94f5681ac97b3f7f64d6a72f6554bdafe7f4e003
Antonis Keremidis <tkeremidis@gmail.com>
2023-07-04 16:20:09 +0300

    Update comments

--- Exercise 3/demo.py
+++ Exercise 3/demo.py
@@ -9,11 +9,11 @@ from exercise_2_functions import camera_looking_at
 class PhongMaterial:
     def __init__(self, ka, kd, ks, n):
         """
-        Initializes a Phong material
-        :param ka: The ambient coefficient
-        :param kd: The diffusion coefficient
-        :param ks: The specular coefficient
-        :param n: The Phong exponent
+        Initializes a Phong material.
+        :param ka: The ambient coefficient.
+        :param kd: The diffusion coefficient.
+        :param ks: The specular coefficient.
+        :param n: The Phong exponent.
         """
         self.ka = ka
         self.kd = kd
@@ -24,9 +24,9 @@ class PhongMaterial:
 class PointLight:
     def __init__(self, position, intensity):
         """
-        Initializes a point light object
-        :param position: A (3,1) array representing the position of the light
-        :param intensity: A (3,1) array representing the intensity of the light for each color
+        Initializes a point light object.
+        :param position: A (3,1) array representing the position of the light.
+        :param intensity: A (3,1) array representing the intensity of the light for each color.
         """
         self.position = position
         self.intensity = intensity
@@ -34,10 +34,10 @@ class PointLight:
 
 def calculate_normals(verts, faces):
     """
-    Calculates the normal vectors for each face
-    :param verts: The vertices of the object
-    :param faces: The faces of the object
-    :return: The normal vectors for each face
+    Calculates the normal vectors for each face.
+    :param verts: The vertices of the object.
+    :param faces: The faces of the object.
+    :return: The normal vectors for each face.
     """
     normals = np.zeros((faces.shape[0], 3))
 
@@ -57,14 +57,14 @@ def calculate_normals(verts, faces):
 def light(point, normal, color, cam_pos, mat, lights):
     """
     Calculates the lighting of a point, which belongs to a Phong material, given the diffused, specular and reflected
-    light
-    :param point: A (3,1) array representing the point
-    :param normal: The (3,1) vector of the surface normal
-    :param color: The color of the point
-    :param cam_pos: The position of the camera
-    :param mat: The Phong material
-    :param lights: a list of PointLight objects
-    :return: The intensity of the color of the point
+    light.
+    :param point: A (3,1) array representing the point.
+    :param normal: The (3,1) vector of the surface normal.
+    :param color: The color of the point.
+    :param cam_pos: The position of the camera.
+    :param mat: The Phong material.
+    :param lights: a list of PointLight objects.
+    :return: The intensity of the color of the point.
     """
     I = np.zeros((1, 3))
 
@@ -91,17 +91,17 @@ def light(point, normal, color, cam_pos, mat, lights):
 
 def shade_gouraud(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, light_amb, img):
     """
-    Shades the object using the Gouraud shading method
-    :param verts_p:
-    :param verts_n:
-    :param verts_c:
-    :param bcoords: The barycentric coordinates of the point
-    :param cam_pos: The position of the camera
-    :param mat: The Phong material
-    :param lights: A list of PointLight objects
-    :param light_amb: The ambient light
-    :param img: The image
-    :return: The image with the object shaded
+    Shades the object using the Gouraud shading method.
+    :param verts_p: The points of the vertices.
+    :param verts_n: The normals of the vertices.
+    :param verts_c: The colors of the vertices.
+    :param bcoords: The barycentric coordinates of the point.
+    :param cam_pos: The position of the camera.
+    :param mat: The Phong material.
+    :param lights: A list of PointLight objects.
+    :param light_amb: The ambient light.
+    :param img: The image.
+    :return: The image with the object shaded.
     """
 
     ...
@@ -110,17 +110,17 @@ def shade_gouraud(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, ligh
 
 def shade_phong(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, light_amb, img):
     """
-    Shades the object using the Phong shading method
-    :param verts_p:
-    :param verts_n:
-    :param verts_c:
-    :param bcoords: The barycentric coordinates of the point
-    :param cam_pos: The position of the camera
-    :param mat: The Phong material
-    :param lights: A list of PointLight objects
-    :param light_amb: The ambient light
-    :param img: The image
-    :return: The image with the object shaded
+    Shades the object using the Phong shading method.
+    :param verts_p: The points of the vertices.
+    :param verts_n: The normals of the vertices.
+    :param verts_c: The colors of the vertices.
+    :param bcoords: The barycentric coordinates of the point.
+    :param cam_pos: The position of the camera.
+    :param mat: The Phong material.
+    :param lights: A list of PointLight objects.
+    :param light_amb: The ambient light.
+    :param img: The image.
+    :return: The image with the object shaded.
     """
 
     ...
@@ -130,24 +130,24 @@ def shade_phong(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, light_
 def render_object(shader, focal, eye, lookat, up, bg_color, M, N, H, W, verts, vert_colors, faces, mat, n, lights,
                   light_amb):
     """
-    Renders an object given the vertices, faces, material and lights
-    :param shader: The shader to use
-    :param focal: The focal length
-    :param eye: The position of the camera
-    :param lookat: The vector pointed where the camera is looking at
-    :param up: The camera up vector
-    :param bg_color: The background color
-    :param M: The height of the image
-    :param N: The width of the image
-    :param H: The height of the image plane
-    :param W: The width of the image plane
-    :param verts: The vertices of the object
-    :param vert_colors: The colors of the vertices
-    :param faces: The faces of the object
-    :param mat: The Phong material
-    :param n: The Phong exponent
-    :param lights: A list of PointLight objects
-    :param light_amb: The ambient light
+    Renders an object given the vertices, faces, material and lights.
+    :param shader: The shader to use.
+    :param focal: The focal length.
+    :param eye: The position of the camera.
+    :param lookat: The vector pointed where the camera is looking at.
+    :param up: The camera up vector.
+    :param bg_color: The background color.
+    :param M: The height of the image.
+    :param N: The width of the image.
+    :param H: The height of the image plane.
+    :param W: The width of the image plane.
+    :param verts: The vertices of the object.
+    :param vert_colors: The colors of the vertices.
+    :param faces: The faces of the object.
+    :param mat: The Phong material.
+    :param n: The Phong exponent.
+    :param lights: A list of PointLight objects.
+    :param light_amb: The ambient light.
     :return:
     """
     img = np.ones((M, N, 3)) * bg_color
