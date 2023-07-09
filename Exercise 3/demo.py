3a01d48c822ddd816d16bafe715420c99e02bf0e
Antonis Keremidis <tkeremidis@gmail.com>
2023-07-09 18:03:35 +0300

    Add shade_phong implementation

--- Exercise 3/demo.py
+++ Exercise 3/demo.py
@@ -121,22 +121,73 @@ def shade_gouraud(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, ligh
     return img
 
 
-def shade_phong(verts_p, verts_n, verts_c, bcoords, cam_pos, mat, lights, light_amb, img):
-    """
-    Shades the object using the Phong shading method.
-    :param verts_p: The points of the vertices.
-    :param verts_n: The normals of the vertices.
-    :param verts_c: The colors of the vertices.
-    :param bcoords: The barycentric coordinates of the point.
-    :param cam_pos: The position of the camera.
-    :param mat: The Phong material.
-    :param lights: A list of PointLight objects.
-    :param light_amb: The ambient light.
-    :param img: The image.
-    :return: The image with the object shaded.
-    """
+def shade_phong(vertices, normals, colors, bcoords, cam_pos, mat, lights, light_amb, img):
+    edges = get_edges_from_vertices(vertices)
+    y_min = min(edges, key=lambda e: e.min_y).min_y
+    y_max = max(edges, key=lambda e: e.max_y).max_y
 
-    ...
+    active_edges = np.array([], dtype=Edge)
+
+    for y in range(y_min, y_max + 1):
+        # Set active edges
+        for edge in edges:
+            # If the edge is not horizontal and the start of the edge is at the current y
+            if edge.min_y == y and edge.max_y != edge.min_y:
+                active_edges = np.append(active_edges, edge)
+            # If the end of the edge is at the current y
+            elif edge.max_y == y:
+                active_edges = np.delete(active_edges, np.where(active_edges == edge))
+
+        # If there are less than 2 active edges, then the triangle is not visible
+        if len(active_edges) < 2:
+            return img
+
+        active_vertices = np.empty((0, 2))
+
+        for active_edge in active_edges:
+            # Get the intersecting x value of the edge at the current y
+            x = active_edge.get_intersecting_x(y)
+            active_vertices = np.vstack((active_vertices, np.array([x, y])))
+
+        active_vertices = np.sort(active_vertices, axis=0)
+        color1_a_index = \
+            np.equal(vertices, (active_edges[0].x1, active_edges[0].y1)).all(axis=1).nonzero()[0].tolist()[0]
+        color2_a_index = \
+            np.equal(vertices, (active_edges[0].x2, active_edges[0].y2)).all(axis=1).nonzero()[0].tolist()[0]
+
+        # Interpolate the colors of the vertices in the y direction
+        color_a = interpolate_vectors((active_edges[0].x1, active_edges[0].y1),
+                                      (active_edges[0].x2, active_edges[0].y2), colors[color1_a_index],
+                                      colors[color2_a_index], y, dim=2)
+        normal_a = interpolate_vectors((active_edges[0].x1, active_edges[0].y1),
+                                       (active_edges[0].x2, active_edges[0].y2), normals[color1_a_index],
+                                       normals[color2_a_index], y, dim=2)
+
+        color1_b_index = \
+            np.equal(vertices, (active_edges[1].x1, active_edges[1].y1)).all(axis=1).nonzero()[0].tolist()[0]
+        color2_b_index = \
+            np.equal(vertices, (active_edges[1].x2, active_edges[1].y2)).all(axis=1).nonzero()[0].tolist()[0]
+        color_b = interpolate_vectors((active_edges[1].x1, active_edges[1].y1),
+                                      (active_edges[1].x2, active_edges[1].y2), colors[color1_b_index],
+                                      colors[color2_b_index], y, dim=2)
+        normal_b = interpolate_vectors((active_edges[1].x1, active_edges[1].y1),
+                                       (active_edges[1].x2, active_edges[1].y2), normals[color1_b_index],
+                                       normals[color2_b_index], y, dim=2)
+
+        for x in range(0, len(active_vertices) - 1, 2):
+            # Get the x values of the vertices
+            x1 = int(active_vertices[x][0])
+            x2 = int(active_vertices[x + 1][0])
+
+            # Interpolate the colors of the vertices in the x direction
+            color = interpolate_vectors((x1, y), (x2, y), color_a, color_b, x, dim=1)
+            normal = interpolate_vectors((x1, y), (x2, y), normal_a, normal_b, x, dim=1)
+
+            # Set the color of the pixels between the two vertices
+            min_x = min(x1, x2)
+            max_x = max(x1, x2)
+            img[img.shape[0] - y, min_x:max_x, :] = light(bcoords, normal, color, cam_pos, mat, lights, light_amb)
+
     return img
 
 
